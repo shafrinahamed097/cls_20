a6771570f87482e27361ce2a0098c585d20c125a
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-06-28 12:02:23 +0600

    Create a view and connect route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,11 +13,16 @@ Route::get('/', function () {
 
 // });
 
-Route::get("/hello", [WelcomeController::class, 'sayhello']);
 
 Route::get('/hello/from/ostad', function(){
     return "Hello From Ostad new Route";
 
 });
 
+
+Route::get("/hello", [WelcomeController::class, 'sayhello']);
 Route::get('/course', [WelcomeController::class, 'courseName']);
+Route::get('/dashboard',[DashboardController::class, 'dashboard']);
+
+
+
